c43b21633ea1bfc18b1006ad03669f476eb0b0df
tangchi <chitang@tujia.com>
2019-12-12 23:14:59 +0800

    feat: rotate_array

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,5 +1,6 @@
 class Solution {
-    static func removeDuplicates(_ nums: inout [Int]) -> Int {
+    
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
         let numsCount = nums.count
         if numsCount == 0 || numsCount == 1 {
             return numsCount
@@ -20,10 +21,55 @@ class Solution {
         }
         return resultCount
     }
+    
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        var hashMap:[String:String] = [:]
+        var max = 0
+        for v in s {
+            if hashMap.keys.contains(where: {$0 == v.description}) {
+                max = 0
+                continue
+            }
+            hashMap[v.description] = v.description
+            max+=1
+        }
+        return max
+    }
+    
+    func rotate(_ nums: inout [Int], _ k: Int) {
+        if (nums.count <= 1) {
+            return
+        }
+        let rotateCount = k % nums.count; //这一步很重要
+        
+        reverse(&nums, start: 0, end: nums.count - 1)
+        reverse(&nums, start: 0, end: rotateCount - 1)
+        reverse(&nums, start: rotateCount, end: nums.count - 1)
+    }
+    
+    private func reverse(_ nums: inout [Int], start: Int, end: Int) {
+        var start = start, end = end
+        while start < end {
+            let temp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = temp
+            start += 1
+            end -= 1
+        }
+    }
 }
 
 
 //var nums = [1,1,2,2,3,3,4,5,6,7,7,8,9,9]
 var nums = [1,2,3]
-let count = Solution.removeDuplicates(&nums)
+let solution = Solution()
+let count = solution.removeDuplicates(&nums)
 print(count)
+let str = "abcabcaa"
+let strCount = solution.lengthOfLongestSubstring(str)
+print(strCount)
+
+var rotate = [1,2,3,4,5,6,7]
+let k = 3
+solution.rotate(&rotate, k)
+print(rotate)
