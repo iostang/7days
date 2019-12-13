b9897fd6e53900a5f1458322552dd9c891e71af8
tangchi <chitang@tujia.com>
2019-12-13 23:37:55 +0800

    feat: merge-two-sorted-lists

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,3 +1,12 @@
+
+class ListNode {
+    let val:Int
+    var next: ListNode?
+    init(_ val:Int) {
+        self.val = val
+    }
+}
+
 class Solution {
     
     /// 删除有序数组重复元素
@@ -48,6 +57,38 @@ class Solution {
             end -= 1
         }
     }
+    
+    func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+//        guard let l1 = l1 else { return l2 }
+//        guard let l2 = l2 else { return l1 }
+//
+//        if (l1.val < l2.val) {
+//            l1.next = mergeTwoLists(l1.next, l2)
+//            return l1
+//        } else {
+//            l2.next = mergeTwoLists(l1, l2.next)
+//            return l2
+//        }
+        
+        let dummy:ListNode? = ListNode(-1)
+        var prev = dummy
+        var l1 = l1
+        var l2 = l2
+        while l1 != nil && l2 != nil {
+            if l1!.val <= l2!.val {
+                prev?.next = l1
+                l1 = l1!.next
+            } else {
+                prev?.next = l2
+                l2 = l2!.next
+            }
+            prev = prev?.next
+        }
+        
+        prev?.next = l1 == nil ? l2 : l1
+        return dummy?.next
+    }
+
 }
 
 let solution = Solution()
@@ -60,3 +101,13 @@ var rotate = [1,2,3,4,5,6,7]
 let k = 3
 solution.rotate(&rotate, k)
 print(rotate)
+
+var l1 = ListNode(1)
+var l2 = ListNode(3)
+var l3 = ListNode(2)
+var l4 = ListNode(4)
+l1.next = l2
+l3.next = l4
+
+let l = solution.mergeTwoLists(l1, l3)
+print(l?.next?.next?.next?.val ?? -1)
